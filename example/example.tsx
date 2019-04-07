2b1bdcc3c0d58c59c9cf72e7547b033cde45c88e
seventeen <1253478653@qq.com>
2019-04-07 16:46:42 +0800

    新增部分icon

--- example/example.tsx
+++ example/example.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from '../lib/components/icon/icon';
 
-ReactDOM.render(<Icon name="qq"/>, document.querySelector('#app'));
+ReactDOM.render(<Icon name="waiting"/>, document.querySelector('#app'));
