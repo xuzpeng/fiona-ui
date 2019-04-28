5ad004940f6fd085c93909bf8904569f7e3d4b2b
seventeen <1253478653@qq.com>
2019-04-28 22:36:04 +0800

    添加input组件例子

--- example/pages/input/input.example.tsx
+++ example/pages/input/input.example.tsx
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
-import { Input } from '../../../lib'; 
+import { Input } from '../../../lib';
+import './input.example.scss';
 
 export default () => {
   const [inputVal, setInputVal] = useState();
   return (
     <div>
-      <Input
-        placeholder="Basic usage" 
-        onChange={(e) => setInputVal(e.currentTarget.value)} 
-        value={inputVal}
-      />
+      <div className="input-example-demo">
+        <h1 className="input-example-h1">基本使用</h1>
+        <Input
+          placeholder="Basic usage"
+          onChange={(e) => setInputVal(e.currentTarget.value)}
+          value={inputVal}
+        />
+      </div>
+      <div className="input-example-demo">
+        <h1 className="input-example-h1">设置大小</h1>
+        <Input
+          placeholder="Basic usage"
+          size="small"
+          onChange={(e) => setInputVal(e.currentTarget.value)}
+          value={inputVal}
+        />
+        <Input
+          placeholder="Basic usage"
+          onChange={(e) => setInputVal(e.currentTarget.value)}
+          value={inputVal}
+        />
+        <Input
+          placeholder="Basic usage"
+          size="large"
+          onChange={(e) => setInputVal(e.currentTarget.value)}
+          value={inputVal}
+        />
+      </div>
     </div>
   )
 }
