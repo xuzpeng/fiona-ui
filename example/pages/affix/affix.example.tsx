beb8582ededf6bb2eb7ae28c2e855ba83a1a301d
seventeen <1253478653@qq.com>
2019-05-08 21:26:34 +0800

    官网新增affix组件页面，组件未完成

--- example/pages/affix/affix.example.tsx
+++ example/pages/affix/affix.example.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Affix } from '../../../lib';
+import React, {useState} from 'react';
+import {Affix, Button} from '../../../lib';
+import './affix.example.scss';
 
 export default () => {
+  const [top] = useState(10)
   return (
-    <div>
+    <div className="f-demo-affix">
       <div>
         <h1>第一个例子</h1>
-        <Affix />
+        <Affix offsetTop={top}>
+          <Button
+            type="primary"
+          >
+            Affix top
+          </Button>
+        </Affix>
       </div>
     </div>
   )
--- lib/components/Affix/affix.tsx
+++ lib/components/Affix/affix.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import './affix.scss';
 
 interface IProps {
-
+  offsetTop?: number
 }
 
-const Affix: React.FC<IProps> = () => {
+const Affix: React.FC<IProps> = ({
+  children, offsetTop
+}) => {
+  const affixTop = useRef(null);
   return (
-    <div>affix</div>
+    <div
+      className='f-affix'
+      ref={affixTop}
+      style={{top: offsetTop}}
+    >{children}</div>
   )
 };
 
