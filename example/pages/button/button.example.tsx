d94a4e9df4da29e81059a8f2d08fcce8d3bb1c18
seventeen <1253478653@qq.com>
2019-05-21 23:29:58 +0800

    新增button基本用法

--- example/pages/button/button.example.tsx
+++ example/pages/button/button.example.tsx
@@ -5,10 +5,12 @@ import './button.example.scss';
 export default () => {
   return (
     <div className='f-btn-demo'>
+      <h1>基本用法</h1>
       <Button className="f-btn" type="primary">Primary</Button>
       <Button className="f-btn" type="default">Default</Button>
       <Button className="f-btn" type="dashed">dashed</Button>
       <Button className="f-btn" type="danger">Danger</Button>
+      <Button type="link">Link</Button>
     </div>
   )
 }
