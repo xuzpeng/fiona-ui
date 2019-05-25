12e993f6d7bb8eb1b1d3ad65c9e7065a8b7a24c4
Code.X <1253478653@qq.com>
2019-05-26 00:07:07 +0800

    loading组件添加文字

--- example/pages/loading/loading.example.tsx
+++ example/pages/loading/loading.example.tsx
@@ -13,6 +13,16 @@ export default () => {
         <Loading size="small" className="f-loading-demo-icon" name="spin"/>
         <Loading size="medium" className="f-loading-demo-icon" name="spin"/>
         <Loading size="large" className="f-loading-demo-icon" name="spin"/>
+        <Loading size="large" className="f-loading-demo-icon" name="spin"/>
+      </div>
+      <div className="f-loading-demo">
+        <h1>第二个例子</h1>
+        <Loading size="small" message="loading..." className="f-loading-demo-icon" name="loading"/>
+        <Loading size="medium" message="加载中..." className="f-loading-demo-icon" name="loading"/>
+        <Loading size="large" message="加载中..." className="f-loading-demo-icon" name="loading"/>
+        <Loading size="small" message="loading..." className="f-loading-demo-icon" name="spin"/>
+        <Loading size="medium" message="加载中..." className="f-loading-demo-icon" name="spin"/>
+        <Loading size="large" message="加载中..." className="f-loading-demo-icon" name="spin"/>
       </div>
     </div>
   )
--- lib/components/loading/loading.tsx
+++ lib/components/loading/loading.tsx
@@ -6,20 +6,26 @@ import combineClass from '../../helpers/combineClass';
 interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string;
   size?: string;
+  message?: string;
 }
 
 const Loading: React.FC<IProps> = (props) => {
   return (
-    <div className={combineClass('f-loading', `f-loading-${props.name}`, `f-loading-${props.size}`, props.className)}>
+    <div className={combineClass('f-loading-container', props.className)}>
+      <div className={combineClass('f-loading', `f-loading-${props.name}`, `f-loading-${props.size}`)}>
+        {
+          props.name === 'spin' && (
+            <Fragment>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+            </Fragment>
+          )
+        }
+      </div>
       {
-        props.name === 'spin' && (
-          <Fragment>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-          </Fragment>
-        )
+        props.message && <div className="f-loading-text">{props.message}</div>
       }
     </div>
   )
