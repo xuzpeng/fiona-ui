f91534c09bbbf9e7aaba49f32e294c646dfce999
Code.X <1253478653@qq.com>
2019-05-27 23:16:23 +0800

    loading组件新增loading属性

--- lib/components/loading/loading.tsx
+++ lib/components/loading/loading.tsx
@@ -1,34 +1,47 @@
 import React, { Fragment } from 'react';
 import './loading.scss';
 import combineClass from '../../helpers/combineClass';
-
+import PropTypes from 'prop-types';
 
 interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string;
   size?: string;
   message?: string;
+  loading?: boolean;
 }
 
 const Loading: React.FC<IProps> = (props) => {
   return (
-    <div className={combineClass('f-loading-container', props.className)}>
-      <div className={combineClass('f-loading', `f-loading-${props.name}`, `f-loading-${props.size}`)}>
-        {
-          props.name === 'spin' && (
-            <Fragment>
-              <i></i>
-              <i></i>
-              <i></i>
-              <i></i>
-            </Fragment>
-          )
-        }
-      </div>
+    <Fragment>
       {
-        props.message && <div className="f-loading-text">{props.message}</div>
+        props.loading && <div className={combineClass('f-loading-container', props.className)}>
+          <div className={combineClass('f-loading', `f-loading-${props.name}`, `f-loading-${props.size}`)}>
+            {
+              props.name === 'spin' && (
+                <Fragment>
+                  <i></i>
+                  <i></i>
+                  <i></i>
+                  <i></i>
+                </Fragment>
+              )
+            }
+          </div>
+          {
+            props.message && <div className="f-loading-text">{props.message}</div>
+          }
+        </div>
       }
-    </div>
+    </Fragment>
   )
+}
+
+Loading.propTypes = {
+  loading: PropTypes.bool
+}
+
+Loading.defaultProps = {
+  loading: true
 }
 
 export default Loading;
