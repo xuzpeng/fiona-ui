83603f5c80344910100774bc770d4f380fb200f6
Code.X <1253478653@qq.com>
2019-06-03 23:21:50 +0800

    修改展示代码按钮

--- example/pages/button/button.demo.tsx
+++ example/pages/button/button.demo.tsx
@@ -1,17 +1,34 @@
 import React, { Fragment, useState } from 'react';
 import ButtonExample from './button.example';
-import { Button } from '../../../lib';
 import CodeEditor from '../../CodeEditor';
+
+const buttonStyle = {
+  marginTop: 20, 
+  width: 40, 
+  height: 40,
+  display: 'flex',
+  borderRadius: '50%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  opacity: 0.3,
+  cursor: 'pointer',
+  backgroundColor: '#ccc',
+  fontSize: 12
+};
 
 export default () => {
   const [y, setY] = useState(0);
   const [rawCode, setRawCode] = useState(require('!!raw-loader!./button.example.tsx').default);
   return <Fragment>
     <ButtonExample />
-    <Button
+    {/* <Button
       onClick={() => setY(y === 0 ? 1 : 0)}
       style={{ marginTop: 20 }}
-    >代码展示</Button>
+    >代码展示</Button> */}
+    <span 
+      onClick={() => setY(y === 0 ? 1 : 0)}
+      style={buttonStyle}
+    >&lt;&nbsp;&gt;</span>
     <CodeEditor scaleY={y} value={rawCode} setRawCode={setRawCode}/>
   </Fragment>
 }
