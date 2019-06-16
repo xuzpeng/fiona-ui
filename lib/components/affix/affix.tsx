7105ce3c345600d3ec49a5f4aa524fab4123dd22
Code.X <1253478653@qq.com>
2019-06-16 17:07:46 +0800

    每个input维护自己的state

--- lib/components/affix/affix.tsx
+++ lib/components/affix/affix.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement, useRef, useState, useEffect} from 'react';
+import React, {ReactElement, useRef, useEffect} from 'react';
 import './affix.scss';
 
 interface IProps {
@@ -8,18 +8,15 @@ interface IProps {
 
 const Affix: React.FC<IProps> = (props) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [fixedOffsetTop, setFixedOffsetTop] = useState(0);
   useEffect(() => {
-    if (!ref.current) return;
     const scrollWindow = () => {
-      
-    }
-    setFixedOffsetTop(ref.current.offsetTop);
+      if (!ref.current) return;
+    };
     window.addEventListener('scroll', scrollWindow);
     return () => {
       window.removeEventListener('scroll', scrollWindow);
     }
-  }, [fixedOffsetTop]);
+  }, []);
   return (
     <div className="f-affix" ref={ref}>{props.children}</div>
   )
